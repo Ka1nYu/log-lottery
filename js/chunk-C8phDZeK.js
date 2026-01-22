fc3e7de527544bd10b8872f809fff6c7ea8b374d
Ka1nYu <Ka1nYu@users.noreply.github.com>
2026-01-22 11:47:39 +0000

    deploy: eb159d6c1c6abae202790a39305c2aeca936582e

---
+++ js/chunk-C8phDZeK.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-qn_g0nUt.js";import{o,Q as s,P as t}from"./chunk-TxbCZCGS.js";const n=r({},[["render",function(r,n){const a=t("router-view");return o(),s(a)}]]);export{n as default};
